447fa0971c8dca2be5c5fa220e02f0243fd6dd92
Jasmine <peachgal99@gmail.com>
2021-10-02 00:41:02 -0400

    initial commit to test in github, if successful, will set repo to private

---
+++ _R/ST558_project1_render_function.R
@@ -0,0 +1,15 @@
+###########################################################
+# ST558 - Vignette Project 1
+# Jasmine Wang
+# Due 10/05/2021
+# 
+# Render function to output .Rmd file for github
+##########################################################3
+
+
+rmarkdown::render("C:/Users/peach/Documents/ST558/ST558_repos/vignette_project/_Rmd/ST558_vignette_proj.Rmd", output_format = "github_document", 
+                  output_file = "C:/Users/peach/documents/ST558/ST558_repos/vignette_project/README.md", 
+                  output_options = list(html_preview = FALSE)
+)
+
+
