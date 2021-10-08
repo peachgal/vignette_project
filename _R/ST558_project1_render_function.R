5ea132acc12b793c55d5e5dde4bfc1ba51834200
Jasmine <peachgal99@gmail.com>
2021-10-07 20:39:31 -0400

    minor change commit 1

--- _R/ST558_project1_render_function.R
+++ _R/ST558_project1_render_function.R
@@ -10,7 +10,7 @@
 rmarkdown::render("C:/Users/peach/Documents/ST558/ST558_repos/vignette_project/_Rmd/ST558_vignette_proj.Rmd", 
                   output_format = "github_document", 
                   output_file = "C:/Users/peach/documents/ST558/ST558_repos/vignette_project/_README/README.md", 
-                  output_options = list(html_preview = FALSE, toc = TRUE, df_print = "tibble")
+                  output_options = list(html_preview = FALSE, toc = TRUE, toc_depth = 3, df_print = "tibble")
 )
 
 
